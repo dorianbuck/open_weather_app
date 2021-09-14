bcc2a51fda5d3bad066fa43933d3567c46ab1a67
Fraser <Fraser.Hughes@btinternet.com>
2021-09-14 20:23:26 +0200

    Refactored test file, added fixture files, started weatherInfo render on App jsx

--- cypress/integration/user_can_get_weather_data.feature.spec.js
+++ cypress/integration/user_can_get_weather_data.feature.spec.js
@@ -1,30 +1,30 @@
 describe("The weather app", () => {
-  it("is expected to display on initial render", () => {
-    // cy.intercept("GET, */weatherData**", { fixture: "weatherData.json" });
-    it("is expected to be displayed on initial render", () => {
-      cy.visit("/", {
-        onBeforeLoad(window) {
-          const stubLocation = {
-            coords: {
-              latitude: 55.5167,
-              longitude: 13,
-            },
-          };
-          cy.stub(window.navigator.geolocation, "getCurrentPosition").callsFake(
-            (callback) => {
-              return callback(stubLocation);
-            }
-          );
-        },
-      });
-      cy.get("[data-cy=weather-display]").within(() => {
-        cy.get("[data-cy=temp]").should("contain", "7°C");
-        cy.get("[data-cy=location]").should("contain", "Stockholm");
-        cy.get("[data-cy=country]").should("contain", "Sweden");
-        cy.get("[data-cy=sunrise]").should("contain", "05:31");
-        cy.get("[data-cy=sunset]").should("contain", "20:01");
-      });
+  beforeEach(() => {
+    cy.intercept("https://api.openweathermap.org/data/2.5/**", {
+      fixture: "weather_response.json",
     });
+    cy.intercept("https://api.opencagedata.com/geocode/v1/json/**", {
+      fixture: "location_response.json",
+    });
+  });
+
+  it("is expected to be displayed on initial render", () => {
+    cy.visit("/", {
+      onBeforeLoad(window) {
+        const stubLocation = {
+          coords: {
+            latitude: 55.5167,
+            longitude: 13,
+          },
+        };
+        cy.stub(window.navigator.geolocation, "getCurrentPosition").callsFake(
+          (callback) => {
+            return callback(stubLocation);
+          }
+        );
+      },
+    });
+
     cy.get("[data-cy=weather-display]").within(() => {
       cy.get("[data-cy=temp]").should("contain", "16.99°C");
       cy.get("[data-cy=location]").should("contain", "Virum");
@@ -33,9 +33,5 @@ describe("The weather app", () => {
 
   it("is expected to display a header", () => {
     cy.get("#header").should("contain", "The Weather App");
-  });
-
-  it("is expected to show a temperature of 16.99°C", () => {
-    cy.get("#temp").should("contain", "16.99°C");
   });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,28 @@ export class App extends Component {
       let { latitude, longitude } = position.coords;
       const apiKey = "f60022513894d605a73019f85cab7c76";
       const ocApiKey = "a0e78a029fc64aac88ff24befed68ab5";
-      let locationResponse = await axios.get(
+      const locationResponse = await axios.get(
         `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${ocApiKey}`
       );
 
-      let weatherResponse = await axios.get(
+      const weatherResponse = await axios.get(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude={,minutely,hourly,daily,alerts}&appid=${apiKey}`
       );
-      let weatherInfo = {
-        city: locationResponse.data.results[0].components.postal_city,
+      const weatherInfo = {
+        city: locationResponse.data.results[0].components.city,
         temp: weatherResponse.data.current.temp,
       };
-      // debugger;
       this.setState({ location: weatherInfo });
+      debugger;
     });
   }
 
   render() {
+    const { weatherInfo } = this.state;
     return (
       <div>
         <h1 id="header">The Weather App</h1>
+        <div>{weatherInfo.city}</div>
       </div>
     );
   }
