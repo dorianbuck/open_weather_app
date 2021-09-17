4d30b89c674b91c7ebcb0f1e5dac070eb895f0bf
Dorian Buck <[dorianbuck@gmail.com]>
2021-09-17 13:58:59 +0200

    adds error msg if user rejects to share location

--- src/App.jsx
+++ src/App.jsx
@@ -9,56 +9,61 @@ export class App extends Component {
   };
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(async (position) => {
-      let { latitude, longitude } = position.coords;
-      const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-      const ocApiKey = process.env.REACT_APP_LOCATION_API_KEY;
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        let { latitude, longitude } = position.coords;
+        const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+        const ocApiKey = process.env.REACT_APP_LOCATION_API_KEY;
 
-      const locationResponse = await axios({
-        method: "GET",
-        url: `https://api.opencagedata.com/geocode/v1/json`,
-        params: { key: `${ocApiKey}`, q: `${latitude}+${longitude}` },
-      });
+        const locationResponse = await axios({
+          method: "GET",
+          url: `https://api.opencagedata.com/geocode/v1/json`,
+          params: { key: `${ocApiKey}`, q: `${latitude}+${longitude}` },
+        });
 
-      const weatherResponse = await axios({
-        mothod: "GET",
-        url: `https://api.openweathermap.org/data/2.5/onecall`,
-        params: {
-          appid: `${apiKey}`,
-          lat: `${latitude}`,
-          lon: `${longitude}`,
-          units: "metric",
-          exclude: `{,minutely,alerts}`,
-        },
-      });
+        const weatherResponse = await axios({
+          mothod: "GET",
+          url: `https://api.openweathermap.org/data/2.5/onecall`,
+          params: {
+            appid: `${apiKey}`,
+            lat: `${latitude}`,
+            lon: `${longitude}`,
+            units: "metric",
+            exclude: `{,minutely,alerts}`,
+          },
+        });
 
-      const weatherInfo = {
-        location: locationResponse.data.results[0].components.city
-          ? locationResponse.data.results[0].components.city
-          : locationResponse.data.results[0].components.county,
-        country: locationResponse.data.results[0].components.country,
-        temp: weatherResponse.data.current.temp,
-        sunrise: new Date(
-          weatherResponse.data.current.sunrise * 1000
-        ).toLocaleTimeString(navigator.language, {
-          hour: "2-digit",
-          minute: "2-digit",
-        }),
-        sunset: new Date(
-          weatherResponse.data.current.sunset * 1000
-        ).toLocaleTimeString(navigator.language, {
-          hour: "2-digit",
-          minute: "2-digit",
-        }),
-        windspeed: weatherResponse.data.current.wind_speed,
-        description: weatherResponse.data.current.weather[0].description,
-      };
+        const weatherInfo = {
+          location: locationResponse.data.results[0].components.city
+            ? locationResponse.data.results[0].components.city
+            : locationResponse.data.results[0].components.county,
+          country: locationResponse.data.results[0].components.country,
+          temp: weatherResponse.data.current.temp,
+          sunrise: new Date(
+            weatherResponse.data.current.sunrise * 1000
+          ).toLocaleTimeString(navigator.language, {
+            hour: "2-digit",
+            minute: "2-digit",
+          }),
+          sunset: new Date(
+            weatherResponse.data.current.sunset * 1000
+          ).toLocaleTimeString(navigator.language, {
+            hour: "2-digit",
+            minute: "2-digit",
+          }),
+          windspeed: weatherResponse.data.current.wind_speed,
+          description: weatherResponse.data.current.weather[0].description,
+        };
 
-      this.setState({ hourlyForcast: weatherResponse.data.hourly });
-      // this.setState({ dailyForcast: weatherResponse.data.daily });
+        this.setState({ hourlyForcast: weatherResponse.data.hourly });
+        // this.setState({ dailyForcast: weatherResponse.data.daily });
 
-      this.setState({ location: weatherInfo });
-    });
+        this.setState({ location: weatherInfo });
+      },
+      (error) => {
+        alert(error.message);
+      }
+    );
   }
 
   render() {
@@ -79,19 +84,15 @@ export class App extends Component {
         <Header color="olive" size="huge" textAlign="center" dividing>
           <h1 id="header">Frasian Weather</h1>
         </Header>
-        <Grid
-          container="text"
-          textAlign="justified"
-          celled="internally"
-        >
-          <div data-cy="weather-current" >
+        <Grid container="text" textAlign="justified" celled="internally">
+          <div data-cy="weather-current">
             <Grid.Row>
               <Grid.Column>
                 <h3>Current Weather</h3>
               </Grid.Column>
             </Grid.Row>
-            <Grid.Row >
-              <Grid.Column > 
+            <Grid.Row>
+              <Grid.Column>
                 <p1 data-cy="location">
                   The current weather in {location}, {country} is {description}.
                 </p1>
@@ -100,7 +101,12 @@ export class App extends Component {
             <Grid.Row>
               <Grid.Column>
                 <p data-cy="temp">
-                  <Icon loading name="thermometer half" color="red" size="big" />
+                  <Icon
+                    loading
+                    name="thermometer half"
+                    color="red"
+                    size="big"
+                  />
                   Temperature: {temp}°C
                 </p>
               </Grid.Column>
@@ -138,7 +144,10 @@ export class App extends Component {
             </Grid.Row>
             <Grid.Row>
               <Grid.Column>
-                <HourlyModal hourlyRain={hourlyForcast} hourlyTemp={hourlyForcast}/>
+                <HourlyModal
+                  hourlyRain={hourlyForcast}
+                  hourlyTemp={hourlyForcast}
+                />
               </Grid.Column>
             </Grid.Row>
           </div>
