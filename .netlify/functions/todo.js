cf5a58d2c1ba05be730397c03bb1b8b8945219ac
Dorian Buck <[dorianbuck@gmail.com]>
2021-09-17 13:35:18 +0200

    adds netlify api secret tokens

---
+++ .netlify/functions/todo.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+exports.handler = async function (event, context) {
+  console.log(event);
+  console.log(context);
+  try {
+    const { id } = event.queryStringParameters;
+    const response = await axios.get(`${process.env.TODO_BASE_URL_WEATHER_API_KEY}/${id}`);
+    const response = await axios.get(`${process.env.TODO_BASE_URL_LOCATION_API_KEY}/${id}`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ title: response.data.title }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 404,
+      body: err.toString(),
+    };
+  }
+};
